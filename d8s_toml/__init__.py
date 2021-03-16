cd392a2306bdce66dbea62d3fe9694ce60ca4845
Floyd Hightower <floyd.hightower27@gmail.com>
2021-03-16 07:12:01 -0500

    Updating imports to reference new, d8s_... names

--- d8s_toml/__init__.py
+++ d8s_toml/__init__.py
@@ -6,10 +6,10 @@ except ImportError:
 from .toml_data import *
 
 try:
-    __version__ = version('democritus_toml')
+    __version__ = version('d8s_toml')
 except PackageNotFoundError:
     message = (
-        'Unable to find a version number for "democritus_toml". '
+        'Unable to find a version number for "d8s_toml". '
         + 'This likely means the library was not installed properly. '
         + 'Please re-install it and, if the problem persists, '
         + 'raise an issue here: https://github.com/democritus-project/democritus-toml/issues.'
